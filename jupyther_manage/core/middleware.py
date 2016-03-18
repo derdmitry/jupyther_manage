4cbfd9e585a5592f5353c93a03446ad57fc94865
Dmitry <derdmitry@gmail.com>
2016-03-18 15:52:23 +0200

    add redirect middleware

--- jupyther_manage/core/middleware.py
+++ jupyther_manage/core/middleware.py
@@ -5,6 +5,6 @@ class UrlRedirectMiddleware:
     def process_request(self, request):
         host = request.META['HTTP_HOST'] + request.META['PATH_INFO']
         if 'HTTP_REFERER' in request.META:
-            if 'proxy' in request.META['HTTP_REFERER']:
-                redirect_url = request.META['HTTP_REFERER'].replace('proxy', 'rewrite') + '/path/' + request.path
+            if 'proxy' in request.META['HTTP_REFERER'] and not 'rewrite' in request.path:
+                redirect_url = request.META['HTTP_REFERER'].replace('proxy', 'rewrite') + 'path' + request.path
                 return HttpResponsePermanentRedirect(redirect_url)
